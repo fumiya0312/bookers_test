cfd96bdaca6912b499603082f9c527298a0eaa5c
ZED <42907172+fumiya0312@users.noreply.github.com>
2021-11-08 18:37:32 +0900

    Revert "Develop"

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,54 @@
 class BooksController < ApplicationController
     def index
-      @books = Book.all
-      @book = Book.new
-      @user = current_user
+        @books = Book.all
+        @book = Book.new
+        @user = current_user
     end
-
+    
     def show
         @book = Book.find(params[:id])
         @user = @book.user
         @new_book = Book.new # showページでbookを作成しようとした場合にupdateが走らないように
     end
-
+    
     def create
-      @book = Book.new(book_params)
-      @book.user_id = current_user.id
-
-      if @book.save
-        redirect_to book_path(@book.id), notice: "You have created book successfully."
-      else
-        @user = current_user
-        @books = Book.all
-        render :index
-      end
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        if @book.save
+            redirect_to book_path(@book.id), notice: "You have created book successfully."
+        else
+            @user = current_user
+            @books = Book.all
+            render :index
+        end
+        
     end
-
+    
     def edit
-      @book = Book.find(params[:id])
-
-      unless @book.user == current_user
-        redirect_to books_path
-      end
+        @book = Book.find(params[:id])
+        unless @book.user == current_user
+            redirect_to books_path
+        end
     end
-
+    
     def update
-      @book = Book.find(params[:id])
-
-      if @book.update(book_params)
-          redirect_to book_path(@book.id), notice: "You have updated book successfully."
-      else
-          render :edit
-      end
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to book_path(@book.id), notice: "You have updated book successfully."
+        else
+            render :edit
+        end
     end
-
+    
     def destroy
         book = Book.find(params[:id])
         book.destroy
         redirect_to books_path
     end
-
+    
     private
     def book_params
         params.require(:book).permit(:title, :body)
     end
+    
 end
